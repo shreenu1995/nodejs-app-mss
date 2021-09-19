dca6c2062edb0004bd867d13f5414ecd644bef67
shreenu1995 <65650373+shreenu1995@users.noreply.github.com>
2021-09-19 20:27:07 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -7,7 +7,7 @@ sonarqubeScanner({
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
             'sonar.projectVersion':'1.0',
-	    'sonar.login': 'admin',
+	    'sonar.login': '4701cc44d7ed13c92c47197298921e4e3be0c40c',
 	    'sonar.password': 'admin',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
